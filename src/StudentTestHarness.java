3c7c56135a0afd34eb737f64a9a7afc7b50126b0
Stephanie Caliwag <stephanie.caliwag@gmail.com>
2021-02-09 10:59:02 -0500

    Did the test harness for the arraylist for interests

--- src/StudentTestHarness.java
+++ src/StudentTestHarness.java
@@ -1,14 +1,26 @@
 /*
     Application Purpose: Student info
     Author: Stephanie Caliwag
-    Date: 2/2/21
-    Time: 6:08pm
+    Date: 2/8/21
+    Time: 9:00pm
  */
 
 
-import java.util.Scanner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class StudentTestHarness {
-    public static void main (String[] args){
+    public static void main (String[] args) {
+        ArrayList<String> personalInterests = new ArrayList<>();
+        personalInterests.addAll(Arrays.asList("Video games", "Photography"));
 
+        Student st1 = new Student("Stephanie", "Caliwag", personalInterests, 200462383);
+        System.out.printf(st1.getFirstName());
+        System.out.println(st1.getLastName());
+        System.out.println(st1.getInterests());
+        System.out.println(st1.getStudentNumber());
+
+    }
 }
